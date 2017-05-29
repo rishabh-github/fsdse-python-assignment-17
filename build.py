24598c7f82a1d18c125f416a2a2bbc24ec1921c1
rishabh-github <rishabh-github@users.noreply.github.com>
2017-05-29 15:19:22 +0000

    Done

--- build.py
+++ build.py
@@ -1,14 +1,17 @@
-import operator
+import operator as op
 
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
 
+    sorted_x = sorted(dic.items(), key=op.itemgetter(0))
+
+    return sorted_x
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
+    sorted_x = sorted(dic.items(), key=op.itemgetter(0),reverse=True)
+    
+    return sorted_x
 
+print solution_asc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
+
+print solution_desc({1: 2, 3: 4, 4: 3, 2: 1, 0: 0})
